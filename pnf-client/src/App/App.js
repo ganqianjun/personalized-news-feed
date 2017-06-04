552c97e74f5e831d23dab0f24acabc63b799e454
ganqianjun <ganqianjun@outlook.com>
2017-06-04 14:57:21 -0700

    Client : Add materialize-css and import

--- pnf-client/src/App/App.js
+++ pnf-client/src/App/App.js
@@ -1,3 +1,6 @@
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
