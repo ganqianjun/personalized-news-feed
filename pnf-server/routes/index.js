0df85eef2da61ca225dbe92bfa66e42adbb45865
ganqianjun <ganqianjun@outlook.com>
2017-06-05 00:35:40 -0700

    Server : Simplify and change somd default files

--- pnf-server/routes/index.js
+++ pnf-server/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile("index.html", { root:path.join(__dirname, '../../pnf-client/build')});
 });
 
 module.exports = router;
