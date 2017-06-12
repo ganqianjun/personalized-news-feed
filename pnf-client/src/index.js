12541bd99473191fb1c3bf183afef225fd1a033e
ganqianjun <ganqianjun@outlook.com>
2017-06-12 01:22:06 -0700

    Client : Add routes

--- pnf-client/src/index.js
+++ pnf-client/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App/App';
+
+import {Router, browserHistory} from 'react-router';
+import routes from './routes';
+
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router history={browserHistory} routes={routes} />,
+  document.getElementById('root')
+);
