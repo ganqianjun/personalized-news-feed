aaac863ce6a91c860946e1aaf608c51a57a1440d
ganqianjun <ganqianjun@outlook.com>
2017-06-25 02:25:12 -0700

    Web_server: Add router and rpc_client method for 'logNewsClickForUser'

--- pnf-server/routes/news.js
+++ pnf-server/routes/news.js
@@ -10,8 +10,17 @@ router.get('/userId/:userId/pageNum/:pageNum', function(req, res, next) {
 
   rpc_client.getNewsSummariesForUser(user_id, page_num, function(response) {
     res.json(response);
-  })
-
+  });
 })
+
+// Log new clidk
+router.post('/userId/:userId/newsId/:newsId', function(req,res, next) {
+  console.log('web-server: Logging news click...');
+  user_id = req.params['userId'];
+  news_id = req.params['newsId'];
+
+  rpc_client.logNewsClickForUser(user_id, news_id);
+  res.status(200);
+});
 
 module.exports = router;
